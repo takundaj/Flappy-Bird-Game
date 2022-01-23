436b53187b35591140196b1d4c288366ee4c6a18
Takunda Nyamatore <tjnyamatore@gmail.com>
2022-01-23 20:14:30 +0000

    .

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const wall = document.querySelector(".wall");
 const hole = document.querySelector(".hole");
-const score = document.querySelector(".score")
+const score = document.querySelector(".score");
 let random = 100;
-hole.style.top = `${random}px`
+hole.style.top = `${random}px`;
 let counter = 0;
 
 wall.addEventListener("animationiteration", (e) => {
   random = Math.random() * 300 + 50;
   hole.style.top = `${random}px`;
-  counter++
-  score.textContent = `${counter}`
+  counter++;
+  score.textContent = `${counter}`;
 });
 
 //gravity on character
@@ -19,18 +19,24 @@ let gravity = 0;
 
 const fall = setInterval(() => {
   char.style.top = `${gravity}px`;
-  gravity += 4;
+  gravity += 2;
 
   //game over checker
   wallLeft = parseInt(getComputedStyle(wall).getPropertyValue("left"));
-  console.log(wallLeft)
+  console.log(wallLeft);
   holeTop = random;
   holeBottom = holeTop + 120;
   charTop = parseInt(getComputedStyle(char).getPropertyValue("top"));
   charBottom = parseInt(getComputedStyle(char).getPropertyValue("bottom"));
 
-  if ((gravity >= 470 || gravity < 0) || ((wallLeft < 230 && wallLeft > 150) && (charTop < holeTop || charTop > holeBottom ))) {
-    gameOver()
+  if (
+    gravity >= 470 ||
+    gravity < 0 ||
+    (wallLeft < 230 &&
+      wallLeft > 150 &&
+      (charTop < holeTop || charTop > holeBottom))
+  ) {
+    gameOver();
   }
 }, 10);
 
@@ -43,7 +49,7 @@ game.addEventListener("click", (e) => {
 function gameOver() {
   gravity = 0;
   alert(`Game over! Your score: ${counter}`);
-  location.reload()
+  location.reload();
   counter = 0;
-  score.textContent = `${counter}`
+  score.textContent = `${counter}`;
 }
